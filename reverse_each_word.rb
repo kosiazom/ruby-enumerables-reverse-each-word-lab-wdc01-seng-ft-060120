8d6c2c71dd1815ebef45987b81416c4a08012cd2
Kosi Akporji <kosiazom@gmail.com>
2020-04-29 21:37:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,9 @@ require 'pry'
 def reverse_each_word (string)
   
   string_array = string.split
-  
   string_array.collect do |element|
     
-  string_reverse = "#{element.reverse}"
-     end
-   string_reverse.join(" ")
-   
+    "#{element.reverse}"
+    end
+   string_array.join(" ")
 end
